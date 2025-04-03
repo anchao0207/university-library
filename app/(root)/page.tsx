b3a88aa1817b7944ce751745d6323815422356a1
An Trieu <anchao0207@gmail.com>
2025-04-03 21:33:10 +0000

    home page and books components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,17 @@ import Image from "next/image";
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview></BookOverview>
-      <BookList></BookList>
+      <BookOverview {...sampleBooks[0]}></BookOverview>
+      <BookList
+        title="Latest Book"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      ></BookList>
     </>
   );
 };
